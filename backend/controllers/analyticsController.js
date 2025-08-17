2e34d3d24fdcce708c7d09ac36cf5b950b4f22d0
douaoouria rassim <162766373+Rassimdou@users.noreply.github.com>
2025-08-17 01:27:50 +0100

    add

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -4,9 +4,9 @@ import Product from '../models/Product.js';
 
 
 
-export const ProductsSold = async (req, res) => {
+export const TotalAmount = async (req, res) => {
     try {
-        const ProdSold = await Order.aggregate([
+        const ToTalAmount = await Order.aggregate([
 
             {$match: { status: "completed" }},
             {$group: {
@@ -15,7 +15,7 @@ export const ProductsSold = async (req, res) => {
             }}  
 
         ])
-
+        res.status(200).json({ totalAmount: ToTalAmount});
     } catch (error) {
         res.status(500).json({ message: "Error fetching products sold", error: error.message });
         console.log("Error fetching products sold:", error);
@@ -24,4 +24,33 @@ export const ProductsSold = async (req, res) => {
 
 }
 
-export const 
+
+export const TotalOrders = async (req, res) => {
+    try {
+        const totalOrders = await Order.aggregate([
+
+            
+            {$group: {
+                _id: null,
+                totalOrders: { $sum },
+            }}  
+
+        ])
+res.status(200).json({ totalOrders: totalOrders });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching total orders", error: error.message });
+        console.log("Error fetching total orders:", error);
+    }
+
+
+}
+
+export const TotalUsers = async (req, res) => {
+    try {
+        const totalUsers = await User.countDocuments();
+        res.status(200).json({ totalUsers });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching total users", error: error.message });
+        console.log("Error fetching total users:", error);
+    }
+}
